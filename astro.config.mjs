a49fc0be3bfa21a9efc9056a78d7422bbe502e72
Benjamin Starostka Jakobsen <benjamin.starostka@gmail.com>
2025-08-21 11:06:42 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://pioneer-centre-for-ai.github.io',
+  base: '/supercomputing-workshop/',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [react()]
+});
